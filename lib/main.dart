1f3b6299ee1dcb74b1e771f154fddf5dd4828970
Kenneth Bass <33816169+dishwasher-detergent@users.noreply.github.com>
2025-03-12 16:32:29 -0500

    test

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,14 @@ import 'package:starter_template/utils.dart';
 import 'package:path/path.dart' as p;
 
 Future<dynamic> main(final context) async {
-  final folder = p.join(p.dirname(Platform.script.toFilePath()), '../../../../');
-  context.log(folder);
-  final dir = Directory(folder);
-  final List<FileSystemEntity> entities = await dir.list().toList();
-  context.log(entities);
+    final result = await Process.run('find', ['~', '-name', 'index.html'], 
+      runInShell: true);
+    
+    if (result.exitCode == 0) {
+      context.log(result.stdout);
+    } else {
+      context.error('Error: ${result.stderr}');
+    }
 
   if (context.req.method == 'GET') {
     return context.res.text(getStaticFile('index.html'), 200,
